a1d97b92810b15e183c02fc1bbb048a31ff196df
sunxiao <292904818@qq.com>
2018-03-08 18:51:33 +0800

    add some new function

--- app/src/main/java/com/example/sun/innotext/fragments/FragmentSetting.java
+++ app/src/main/java/com/example/sun/innotext/fragments/FragmentSetting.java
@@ -3,12 +3,14 @@ package com.example.sun.innotext.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.sun.innotext.LoginActivity;
@@ -20,27 +22,32 @@ import com.example.sun.innotext.SettingItem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FragmentSetting extends Fragment implements AdapterView.OnItemClickListener,View.OnClickListener{
+import static android.content.ContentValues.TAG;
+
+public class FragmentSetting extends Fragment implements View.OnClickListener{
 
     private List<SettingItem> settingItemList=new ArrayList<SettingItem>();
-    private Button username;
+    private RelativeLayout relativeLayout;
+    private Button bt_login_logout;
+    private Button bt_fingerprint;
+    private Button bt_log;
+
+    public static boolean isLogin=false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_setting, container, false);
-        initSettings();
+        //initSettings();
         SettingAdapter adapter=new SettingAdapter
                 (getActivity(),settingItemList);
-        ListView listView=view.findViewById(R.id.listview);
+        //ListView listView=view.findViewById(R.id.listview);
         //注册设置列表点技监控
-        listView.setOnItemClickListener(this);
-        listView.setAdapter(adapter);
+        //listView.setOnItemClickListener(this);
+        //listView.setAdapter(adapter);
         //注册用户名点击监控
-        username=view.findViewById(R.id.username);
-        username.setOnClickListener(this);
-
+        setButtonListener(view);
         return view;
     }
 
@@ -53,7 +60,18 @@ public class FragmentSetting extends Fragment implements AdapterView.OnItemClick
         settingItemList.add(signOut);
     }
 
-    @Override
+    private void setButtonListener(View view){
+        relativeLayout=view.findViewById(R.id.user_layout);
+        relativeLayout.setOnClickListener(this);
+        bt_login_logout=view.findViewById(R.id.login_logout);
+        bt_login_logout.setOnClickListener(this);
+        bt_fingerprint=view.findViewById(R.id.fingerprint);
+        bt_fingerprint.setOnClickListener(this);
+        bt_log=view.findViewById(R.id.log);
+        bt_log.setOnClickListener(this);
+    }
+
+    /*@Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         SettingItem item=settingItemList.get(position);
 
@@ -70,15 +88,26 @@ public class FragmentSetting extends Fragment implements AdapterView.OnItemClick
                 startActivity(intent);
                 break;
         }
-    }
+    }*/
 
     @Override
     public void onClick(View v) {
+        Intent intent=null;
         switch (v.getId()){
-            case R.id.username:
-                Intent intent=new Intent(getActivity(),PersonActivity.class);
+            case R.id.user_layout:
+                intent=new Intent(getActivity(),PersonActivity.class);
                 startActivity(intent);
-                Toast.makeText(getActivity(),"clicked user name",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.login_logout:
+                Log.d(TAG, "onClick: you click login button");
+                intent=new Intent(getActivity(),LoginActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.fingerprint:
+                Log.d(TAG, "onClick: you click fingerprint button");
+                break;
+            default:
+                Log.d(TAG, "onClick: you click nothing");
         }
     }
 }
